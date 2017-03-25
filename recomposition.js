b13419ff72ce00f9806269a4797effb2e764eab8
Cecile Bertin <elicec12@gmail.com>
2017-03-25 16:19:21 -0700

    Update recomposition.js
    variable name correction

--- recomposition.js
+++ recomposition.js
@@ -3,9 +3,9 @@
 // output: unicode number representing a hangul character
 
 const recomposition = (arr) => {
-  let UnicodeTotal = (arr[0] * 588) + (arr[1] * 28) + 44032;
+  let unicodeTotal = (arr[0] * 588) + (arr[1] * 28) + 44032;
   if(arr.length === 3) {
-    UnicodeTotal += arr[2];
+    unicodeTotal += arr[2];
   }
-  return UnicodeTotal;
+  return unicodeTotal;
 }
