edc8795c486ea44ff903ad82287426a6102b874b
Bryan Jennings <bryanjenningz@gmail.com>
2017-03-28 14:23:09 -0700

    present-tense hindi tests pass

--- src/hindi.js
+++ src/hindi.js
@@ -1,15 +1,15 @@
 class Hindi {
 
   conjugate (word, info) {
-    // Format for rulesObject: { tense: 'present', formal: 'true/false', wordType: 'adjective/verb'}
-   switch (info.tense) {
-     case 'present':
-       return ''; // Return conjugated word in the present tense
-       break;
-     default:
-     return 'Could not find any rules for conjugation';
-   }
- }
+    // Format for info: { tense: 'present/past', formal: true/false, wordType: 'adjective/verb' }
+    word = word.trim();
+    switch (info.tense) {
+      case 'present':
+        return word.replace(/na$/, 'ta hoon');
+      default:
+        return 'Could not find any rules for conjugation';
+    }
+  }
 }
 
 export { Hindi };
