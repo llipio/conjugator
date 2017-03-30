990c7272e44fdb4fc75ab32e4ed07f6b345e6b78
Dana Ng <ngdana1@gmail.com>
2017-03-30 10:50:25 -0700

    updated with es6 syntax and switch statement - recommended by Song

--- src/korean.js
+++ src/korean.js
@@ -27,53 +27,48 @@ class Korean {
       let brokeLength = brokeWord.length;
       let syllableEnd = brokeWord[brokeLength - 1];
       let stemWord = word.slice(0, wordLength - 2);
-      if(syllableEnd === 0) {
-          // if last letter is ㅏ leave alone
-          return word.slice(0, wordLength - 1);;
-      } else if(syllableEnd === 8) {
-        // replace with: ㅘ (9)
-          // concat back to word
-          let newSyllable = brokeWord.slice(0, brokeLength - 1);
-          newSyllable.push(9);
-          newSyllable = reverseBreakdown(newSyllable);
-          return stemWord + newSyllable;
-      } else if(syllableEnd === 18) {
-      // vowel ㅡ replace with ㅓ (4)
-        let newSyllable = brokeWord.slice(0, brokeLength - 1);
-            newSyllable.push(4);
-            newSyllable = reverseBreakdown(newSyllable);
-          if(wordLength <= 2) {
-            return newSyllable;
-          } else {
-            return stemWord + newSyllable;
-          }
-     } 
-     else if (syllableEnd === 13) {
-      // 13 (ㅜ) convert to 14 (ㅝ)
       let newSyllable = brokeWord.slice(0, brokeLength - 1);
-            newSyllable.push(14);
-            newSyllable = reverseBreakdown(newSyllable);
+      switch(syllableEnd) {
+        case 0: 
+          // if last letter is ㅏ leave alone
+          return word.slice(0, wordLength - 1);
+        case 8: 
+          // replace with: ㅘ (9)
+            // concat back to word
+            newSyllable.push(9);
+            newSyllable = combineSymbols(newSyllable);
+            return stemWord + newSyllable;
+        case 18:
+        // vowel ㅡ replace with ㅓ (4)
+              newSyllable.push(4);
+              newSyllable = combineSymbols(newSyllable);
+            if(wordLength <= 2) {
+              return newSyllable;
+            } else {
+              return stemWord + newSyllable;
+            }
+       case 13:
+        // 13 (ㅜ) convert to 14 (ㅝ)
+          newSyllable.push(14);
+          newSyllable = combineSymbols(newSyllable);
           if(wordLength <= 2) {
             return newSyllable;
           } else {
             return stemWord + newSyllable;
           }
-     }
-     else if(syllableEnd === 20) {
-      // vowel ㅣ replace with ㅕ(6)
-          let newSyllable = brokeWord.slice(0, brokeLength - 1);
+       case 20:
+        // vowel ㅣ replace with ㅕ(6)
           newSyllable.push(6)
-          newSyllable = reverseBreakdown(newSyllable);
+          newSyllable = combineSymbols(newSyllable);
           if(wordLength <= 2) {
             return newSyllable;
           } else {
             return stemWord + newSyllable;
           }
-      } else {
-          // if consonant
-          // need to find consonant
-          conjugate = word.slice(0, wordLength - 1);
-          return conjugate.concat('어');
+        default:
+            // if consonant
+            conjugate = word.slice(0, wordLength - 1);
+            return conjugate.concat('어');
       }
     } // end of first else
   } // end of presentWord function
@@ -89,7 +84,7 @@ class Korean {
 
 /** Additional Functions **/
 
-function breakdown(input){
+const breakdown = (input) => {
     const hex = '\\u' + (input.charCodeAt(0) + 0x10000).toString(16).slice(1);
     const numHex = hex.substring(2);
     let total = parseInt(numHex, 16);
@@ -108,7 +103,7 @@ function breakdown(input){
     return letterArray;
 }
 
-function reverseBreakdown(input) {
+const combineSymbols = (input) => {
     let initialValue = input[0] * 588;
     let medialValue = input[1] * 28;
     let finalValue = input[3] ? input[3] : 0;
