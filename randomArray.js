49173225ab917f8c428ac58cbe04fd289bfb7084
cecile <elicec12@gmail.com>
2017-04-11 14:16:08 -0700

    use a swap instead of split

--- randomArray.js
+++ randomArray.js
@@ -4,11 +4,15 @@ function mixedArray1 (arr) {
   const newArr = _.shuffle(arr);
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === newArr[i]){
-      newArr.splice(i, 1);
-      const randomIndex = Math.floor(Math.random() * newArr.length);
-      const randomElement = newArr[randomIndex];
-      newArr.splice(randomIndex, 1, arr[i]);
-      newArr.splice(i, 0, randomElement);
+      const randomBag = newArr.slice(0,i).concat(newArr.slice(i+1));
+      const randomIndex = Math.floor(Math.random() * randomBag.length);
+      const randomElement = randomBag[randomIndex];
+      newArr[i] = randomElement;
+      if (randomIndex >= i) {
+        newArr[randomIndex + 1] = arr [i];
+      } else {
+        newArr[randomIndex] = arr [i];
+      }
     }
   }
   return newArr;
