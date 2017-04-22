6c2b0b9a8e0495343d06835e56c66ef205e64c16
cecile <elicec12@gmail.com>
2017-04-22 15:36:15 -0700

    improvements: change breaks to returns, replace all stringconcat to template literals

--- src/french.js
+++ src/french.js
@@ -4,21 +4,33 @@ class French {
         TODO: pronominal verbs: 'se' + infinitif
         TODO: passive form: 'être' + past participle
       info: {
-        mood: 'Indicatif'/'Subjonctif'/'Impératif'/'Conditionnel'/'Infinitif'/'Participe'
-        tense: 'present'/'past'/'future'/'simple past'/..., (!!!tenses different from english tenses)
+        mood: 'Indicatif'/'Subjonctif'/'Impératif'/'Conditionnel'/'Infinitif'
+              /'Participe'
+        tense: 'present'/'past'/'future'/'simple past'/...,
+              (!!!tenses different from english tenses)
         person (subject): '1'/'2'/'3',
           '1': Je, nous (I, we),
           '2': tu, vous (you),
-          '3': he, she, they, it, undefined ('on', people, infinitif verb, object, 'ça', 'cela', ...)
-        singular: 'true/false',
-          if undefined subject: 'on' (english: we as a whole, it), people, infinitif verb, qui, tout, rien, object -> is considered singular -> true
-          ! 'on' can also be used instead of 'nous' (but this use is to be avoided) -> is considered plural -> false
+          '3': he, she, they, it, undefined
+              ('on', people, infinitif verb, object, 'ça', 'cela', ...)
+        singular: boolean,
+          if undefined subject: 'on' (english: we as a whole, it), people,
+          infinitif verb, qui, tout, rien, object -> is considered singular
+           -> true
+          !'on' can also be used instead of 'nous' (but is avoided)
+           -> is considered plural -> false
           true -> singular -> 's'
           false -> plural -> 'p'
-        formal: 'true/false',
-          'vous' can be used to adress a singular person in a formal way -> if the subject is 's2' and formal, it is conjugated as 'p2', but the past participle stays singular
+        formal: boolean,
+          'vous' can be used to adress a singular person in a formal way
+          -> if the subject is 's2' (singular 2nd person) and formal,
+             it is conjugated as 'p2'(plural 2nd person),
+             but the past participle stays singular
         gender: 'female/male/unknown',
-          undefined subject: ils (english: they), nous (english: we) , vous (you, adressing a group of persons), the people -> is conjugated as male, when we don't know who 'they', 'nous', 'vous' represent (male or female)
+          undefined subject: ils (english: they), nous (english: we) ,
+          vous (you, adressing a group of persons), the people
+          -> is conjugated as male, when we don't know who 'they', 'nous',
+             'vous' represent (male or female)
           unknown: only for 'on' with the meaning 'it'
           ! 'on' and objects 'can' also have a gender !
       }
@@ -36,7 +48,7 @@ class French {
           case 'Passé Antérieur':
           case 'Futur Simple':
           case 'Futur Antérieur':
-            break;
+            return 'not implemented yet'
           default:
             return 'Indicatif Tense not found';
         }
@@ -44,9 +56,9 @@ class French {
       case 'Infinitif':
         switch (info.tense) {
           case 'Présent':
-           return word;
+            return word;
           case 'Passé':
-            break;
+            return 'not implemented yet'
           default:
             return 'Infinitif Tense not found';
         }
@@ -55,7 +67,7 @@ class French {
         switch (info.tense) {
           case 'Présent':
           case 'Passé':
-            break;
+            return 'not implemented yet'
           default:
             return 'Participe Tense not found';
         }
@@ -65,7 +77,7 @@ class French {
           case 'Passé':
           case 'Imparfait':
           case 'Plus-que-parfait':
-            break;
+            return 'not implemented yet'
           default:
             return 'Subjonctif Tense not found';
         }
@@ -73,7 +85,7 @@ class French {
         switch (info.tense) {
           case 'Présent':
           case 'Passé':
-            break;
+            return 'not implemented yet'
           default:
             return 'Impératif Tense not found';
         }
@@ -82,11 +94,10 @@ class French {
           case 'Présent':
           case 'Passé 1':
           case 'Passé 2':
-            break;
+            return 'not implemented yet'
           default:
-          return 'Conditionnel Tense not found';
+            return 'Conditionnel Tense not found';
         }
-        break;
       default:
         return 'Mood not found';
     }
@@ -95,14 +106,16 @@ class French {
   getSubject (info) {
     // returns the subject for conjugation: 's1'/'s2'/'s3'/'p1'/'p2'/'p3'
     if (info.singular) {
-      if (info.person === '2' && info.formal === true){
-        //if the subject ('you') is meant as formal and singular, it is conjugated as plural, but the past participle stays singular
+      if (info.person === '2' && info.formal === true) {
+        /*if the subject ('you') is meant as formal and singular,
+        it is conjugated as plural, but the past participle stays singular*/
         return 'p2';
       }
       return `s${info.person}`;
     } else {
-      if (info.person === '3' && info.gender === 'unknown'){
-        //if the subject is plural and undefined ('on'), is is conjugated as singular, but the past participle stays plural
+      if (info.person === '3' && info.gender === 'unknown') {
+        /*if the subject is plural and undefined ('on'),
+         is is conjugated as singular, but the past participle stays plural*/
         return 's3';
       }
       return `p${info.person}`;
@@ -110,12 +123,13 @@ class French {
   }
 
   getWordGroup (word) {
-    // returns group1/group2/group3 depending on ending of the infinif form of the verb
-    // group1: all verbs ending in -er except the verb 'aller'
-    // group2: all verbs ending in -ir except the irregular verbs ending in -ir
-    // group3: irregular verbs ending in -ir, -re, -oir
-    let stem = word.slice(0, -2);
-    let ending = word.slice(-2);
+    /* returns group1/group2/group3 depending on ending of the infinif form
+      of the verb
+      group1: all verbs ending in -er except the verb 'aller'
+      group2: all verbs ending in -ir except the irregular verbs ending in -ir
+      group3: irregular verbs ending in -ir, -re, -oir*/
+      let stem = word.slice(0, -2);
+      let ending = word.slice(-2);
 
     if (ending === 'er') {
       return ['group1', stem];
@@ -253,9 +267,10 @@ class French {
   }
 
   combineToConjugate (group, stem, ending) {
-    // the last letter of the stem and the first letter of the tense ending affect each other in some specific cases
-    // in some cases there are several stem solutions
-    // those cases apply to all tenses
+    /* the last letter of the stem and the first letter of the tense ending
+      affect each other in some specific cases
+      in some cases there are several stem solutions
+      those cases apply to all tenses*/
     let combinations = [];
     if (group === 'group1') {
       // rules for group1: verbs ending in -er
@@ -263,55 +278,64 @@ class French {
         // cases of tense endings: 'a' or 'o'
         if (stem.charAt(stem.length - 1) === 'c') {
           // verbs ending in -cer
-          return [stem.slice(0, -1) + 'ç' + ending];
+          return [`${stem.slice(0, -1)}ç${ending}`];
         } else if (stem.charAt(stem.length - 1) === 'g') {
           // verbs ending in -ger
-          return [stem + 'e' + ending];
+          return [`${stem}e${ending}`];
         }
       }
       if (ending.charAt(0) === 'e' && ending.charAt(1) !== 'z') {
         // cases of tense endings: '-e', '-es', '-ent' and '-er(.)'
         const globalPattern = /e[cmnprsvlt]/;
-        if (globalPattern.test(stem.slice(-2)) || stem.slice(-3) === 'evr') {
-          // verbs ending in -e(.)er and (.) can only be either 'c', 'm', 'n', 'p', 'r', 's', 'v', 'vr', 'l', 't'
+        if (globalPattern.test(stem.slice(-2)) || stem.slice(-3) === 'evr') {
+          /* verbs ending in -e(.)er and (.) can only be either
+          'c', 'm', 'n', 'p', 'r', 's', 'v', 'vr', 'l', 't'*/
           if (stem.slice(-2) === 'el' || stem.slice(-2) === 'et') {
             // verbs ending in -eler or -eter
             // special cases of -el and -et that always become -èl and -èt
-            const elExceptions = ['cel', 'décel', 'recel', 'cisel', 'démantel', 'écartel', 'encastel', 'gel', 'dégel', 'congel', 'surgel', 'décongel', 'martel', 'model', 'pel', 'remodel'];
-            const etExceptions = ['achet', 'rachet', 'béguet', 'corset', 'crochet', 'filet', 'furet', 'halet'];
+            const elExceptions = ['cel', 'décel', 'recel', 'cisel', 'démantel',
+                                  'écartel', 'encastel', 'gel', 'dégel',
+                                  'congel', 'surgel', 'décongel', 'martel',
+                                  'model', 'pel', 'remodel'];
+            const etExceptions = ['achet', 'rachet', 'béguet', 'corset',
+                                  'crochet', 'filet', 'furet', 'halet'];
             // special cases of -el and -et that always become -ell and -ett
             const patternCase1 = /jet$/;
             const patternCase2 = /appel$/;
             const case3 = 'interpel';
 
             if(elExceptions.indexOf(stem) !== -1 || etExceptions.indexOf(stem) !== -1) {
-              return [stem.slice(0, -2) + 'è' + stem.slice(-1) + ending];
+              return [`${stem.slice(0, -2)}è${stem.slice(-1) + ending}`];
             } else if (patternCase1.test(stem) || patternCase2.test(stem) || stem === case3) {
-              return [stem + stem.slice(-1) + ending];
+              return [`${stem + stem.slice(-1) + ending}`];
+              //return [stem + stem.slice(-1) + ending];
             } else {
-              //all other cases can have two solutions: (-èl and -èt) or (-ell and -ett)
-              combinations[0] = stem + stem.slice(-1) + ending;
-              combinations[1] = stem.slice(0, -2) + 'è' + stem.slice(-1) + ending;
+              /*all other cases can have two solutions:
+               (-èl and -èt) or (-ell and -ett)*/
+              combinations[0] = `${stem + stem.slice(-1) + ending}`;
+              combinations[1] = `${stem.slice(0, -2)}è${stem.slice(-1) + ending}`;
               return combinations;
             }
           } else if (stem.slice(-3) === 'evr') {
             // verbs ending in -evrer
-            return [stem.slice(0, -3) + 'èvr' + ending];
+            return [`${stem.slice(0, -3)}èvr${ending}`];
           } else {
-            // verbs ending in -e(.)er where (.) is either 'c', 'm', 'n', 'p', 'r', 's', 'v'
-            return [stem.slice(0, -2) + 'è' + stem.slice(-1) + ending];
+            /* verbs ending in -e(.)er where (.) is either
+               'c', 'm', 'n', 'p', 'r', 's', 'v'*/
+            return [`${stem.slice(0, -2)}è${stem.slice(-1) + ending}`];
           }
         }
 
         if (/[aou]y/.test(stem.slice(-2))) {
           //verbs ending in -ayer, -oyer or -uyer
           if (/envoy/.test(stem) && ending.charAt(1) === 'r') {
-            // exceptions: verbs with '-envoy-' in the stem, like envoyer, renvoyer,...
-            return [stem.slice(0, -2) + 'er' + ending.slice(2)];
+            /* exceptions: verbs with '-envoy-' in the stem,
+              like envoyer, renvoyer,...*/
+            return [`${stem.slice(0, -2)}er${ending.slice(2)}`];
           } else {
-            combinations[0] = stem.slice(0,-1) + 'i' + ending;
+            combinations[0] = `${stem.slice(0,-1)}i${ending}`;
             if (stem.charAt(stem.length - 2) === 'a') {
-              combinations[1] = stem + ending;
+              combinations[1] = `${stem + ending}`;
             }
             return combinations;
           }
@@ -321,9 +345,9 @@ class French {
           // cases of endings: '-e', '-es' and '-ent'
           // verbs ending in -é(.)er
           if (stem.substr(-2, 1) === 'é') {
-            return [stem.slice(0, -2) + 'è' + stem.slice(-1) + ending];
+            return [`${stem.slice(0, -2)}è${stem.slice(-1) + ending}`];
           } else if (stem.substr(-3, 1) === 'é'){
-            return [stem.slice(0, -3) + 'è' + stem.slice(-2) + ending];
+            return [`${stem.slice(0, -3)}è${stem.slice(-2) + ending}`];
           }
         }
       }
@@ -404,7 +428,7 @@ class French {
       }
     } else if (group === 'group2') {
       if (/ï$/.test(stem) && /^s/.test(subject)) {
-        stem = stem.slice(0, -1) + 'i';
+        stem = `${stem.slice(0, -1)}i`;
       }
       let solutions = this.combineToConjugate(group, stem, tenseEnding['ending2'][subject]);
       return solutions[0];
