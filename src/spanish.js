552a4e9fda615ceca201594d8f1038f06bee693e
trifrog <tri.d.le@live.com>
2017-05-05 23:45:57 -0700

    Add spanish version of conjugation
    Adjusted the syntax and styles associated with first commit.  This
    version only provides the ability to test present tense and one
    noun version of conjugation.

--- src/spanish.js
+++ src/spanish.js
@@ -1,12 +1,10 @@
 class Spanish {
-  conjugate(word,info){
-
-    let ending = word.slice(word.length-2,word.length);
+  conjugate (word, info) {
     const NOUN_ERROR = 'Please input yo, tû, Ud., él, or ella for the noun';
 
-    switch (info.noun){
+    switch (info.noun) {
       case 'yo':
-        return word.slice(0,-2) + 'o'
+        return `${word.slice(0, -2)}o`;
       default:
         throw new Error(NOUN_ERROR);
     }
