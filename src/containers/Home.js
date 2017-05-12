b7b780549ecfd3a62e4ad5e5d364a4ea01ad5928
trifrog <tri.d.le@live.com>
2017-05-12 16:23:48 -0700

    Issue #60: Add container actions for conjugator page

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,6 +5,18 @@ const generateHomeContainer = connect(() => {
     word: 'speak',
     selectedLanguage: 'Hindi'
   };
+}, (dispatch) => {
+  return {
+    changeLanguage: () => {
+      dispatch({ type: 'CHANGE_LANGUAGE' });
+    },
+    submit: () => {
+      dispatch({ type: 'SUBMIT' });
+    },
+    changeWord: () => {
+      dispatch({ type: 'CHANGE_WORD' });
+    }
+  };
 });
 
 export { generateHomeContainer };
