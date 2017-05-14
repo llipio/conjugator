52100472abcd12a73d2206713aa1976afced0325
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2017-05-13 21:25:11 -0700

    Fix eslint problems

--- src/portuguese.js
+++ src/portuguese.js
@@ -1,42 +1,41 @@
 
-class Portuguese {
-  
-  conjugate(word, info) {
-    switch(info.mood.toLowerCase()) {
-      case 'indicativo': 
-      return new Indicativo().conjugate(word, info);
-      default:
-      return 'unkown';
-    }
-  }
-  
-};
-
-class Indicativo {
-  conjugate(word, info) {
-    switch(info.tense.toLowerCase()) {
-      case 'presente': 
-      return new PresenteIndicativo().conjugate(word, info);
-      default:
-      return 'unkown';
-    }
-  }
-}
-
 class PresenteIndicativo {
-  conjugate(word, info) {
-    var ending = ['amos', 'ais', 'am'];
-    switch(word.slice(-2)) {
-      case "ar":
-        if(info.singular) ending = ['o', 'as', 'a']; 
+  conjugate (word, info) {
+    let ending = ['amos', 'ais', 'am'];
+    switch (word.slice(-2)) {
+      case 'ar':
+        if (info.singular) ending = ['o', 'as', 'a'];
         break;
-      case "er":
-        if(info.singular) ending = ['o', 'es', 'e']
+      case 'er':
+        if (info.singular) ending = ['o', 'es', 'e'];
         else ending = ['emos', 'eis', 'em'];
         break;
+      default:
+        break;
     }
-    return word.slice(0, -2) + ending[parseInt(info.person, 10)-1];
+    return word.slice(0, -2) + ending[parseInt(info.person, 10) - 1];
   }
 }
 
-export {Portuguese};
+class Indicativo {
+  conjugate (word, info) {
+    switch (info.tense.toLowerCase()) {
+      case 'presente':
+        return new PresenteIndicativo().conjugate(word, info);
+      default:
+        return 'unkown';
+    }
+  }
+}
+class Portuguese {
+  conjugate (word, info) {
+    switch (info.mood.toLowerCase()) {
+      case 'indicativo':
+        return new Indicativo().conjugate(word, info);
+      default:
+        return 'unkown';
+    }
+  }
+}
+
+export { Portuguese };
