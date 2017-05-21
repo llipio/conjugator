dd3c225ec05daf551235be55d209d975860945ca
trifrog <tri.d.le@live.com>
2017-05-21 13:00:46 -0700

    Add radio buttons

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../style/App.css';
 import '../style/ReactDropdown.css';
 import Dropdown from 'react-dropdown';
 
 const App = ({options, selectedLanguage, changeLanguage, word, changeWord, languageList, submit, setOptions, info, conjugatedWord}) => {
-  let optionList = [];
-  for(let k in options){
-    optionList.push(<Dropdown key={k} value={info[k]} options={options[k]} placeholder={k} onChange={(a) => {setOptions(k,a.value)}}/>)
-  }
+  let optionListForm = [];
+  let optionBulletForm = [];
+  for(let k in options) {
+    if (options[k].length > 2){
+      optionListForm.push(<Dropdown key={k} value={info[k]} options={options[k]} placeholder={k} onChange={(a) => {setOptions(k,a.value)}}/>)
+    } else {
+      console.log('options[k] = ', options[k]);
+      console.log('k = ', k)
+      options[k].map(element => {
+        console.log('element = ', element);
+        let radioOption = `${element}`;
+        return (optionBulletForm.push(<label key={element}><input key={element} type="radio" name={k} value={element} onChange={(selection) => {setOptions(element,selection.value)}}/>{radioOption}</label>)
+        )})
+    }}
 
   return (
     <div className="App">
@@ -16,22 +26,21 @@ const App = ({options, selectedLanguage, changeLanguage, word, changeWord, langu
       </div>
 
       <Dropdown options={languageList} onChange={(languageOption) => {changeLanguage(languageOption.value)}} value={selectedLanguage} placeholder="Please Select Language" />
-      {optionList}
+      {optionListForm}
+      {optionBulletForm}
       <div>
-        <form>
-          <input type="text" name="word" placeholder="Please input word" value={word} onChange={(e) => {
-            changeWord(e.target.value);
-          }} />
-      </form>
+        <input type="text" name="word" placeholder="Please input word" value={word} onChange={(e) => {
+          changeWord(e.target.value);
+        }} />
     </div>
 
     <div>
       <button type="button" className="btn btn-primary" onClick={submit}>Submit</button>
-  </div>
+    </div>
 
     <h1>{conjugatedWord}</h1>
-    </div>
-);
+  </div>
+  );
 }
 
 export default App;
