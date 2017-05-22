d833c0de7ae02cfd1c7cbf0f17ca8ac912e6ffd5
cecile <elicec12@gmail.com>
2017-05-21 23:48:23 -0700

    normalize info and fix getAllInfo

--- src/hindi.js
+++ src/hindi.js
@@ -1,72 +1,42 @@
 const allInfo = {
-  Tense: ['Present', 'Past', 'Future'],
-  gender: ['Female', 'Male'],
-  formal: ['True', 'False'],
-  'word Type': ['Adjective', 'Verb']
+  tense: ['Present', 'Past', 'Future'],
+  gender: ['female', 'male'],
 };
 
 class Hindi {
-  getInfoList () {
-    return Object.keys(allInfo);
-  }
-
-  getInfoOptions (option) {
-    return allInfo[option];
-  }
-
-  setInfo (infoSite) {
-    const info = {
-      tense: '',
-      gender: 'm',
-      formal: false,
-      wordType: ''
-    };
-    info.tense = infoSite.Tense.toLowerCase();
-    if (infoSite.Gender === 'Female') {
-      info.gender = 'f';
-    }
-    if (infoSite.Formal === 'true') {
-      info.formal = true;
-    }
-    info.wordType = infoSite['Word Type'].toLowerCase();
-    return info;
+  getAllInfo () {
+    return allInfo;
   }
 
   conjugate (word, info) {
     // Format for info:
-    // { tense: 'past/present/future', gender: 'm/f', formal: true/false, wordType: 'adjective/verb' }
-
+    // {tense: 'Past/Present/Future', gender: 'male/female'}
     word = word.trim().toLowerCase();
-    const tense = (info.tense || '').trim().toLowerCase();
-    const gender = (info.gender || '').trim().toLowerCase();
 
-    const TENSE_ERROR = `You entered a tense that we don't know about: "${tense}". The tenses that we support are "past", "present", and "future".`;
-    const GENDER_ERROR = `You entered a gender that we don't know about: "${gender}". The genders that we support are "m" and "f".`;
-
-    switch (tense) {
-      case 'present':
-        switch (gender) {
-          case 'm':
+    switch (info.tense) {
+      case 'Present':
+        switch (info.gender) {
+          case 'male':
             return word.replace(/na$/, 'ta hun');
-          case 'f':
+          case 'female':
             return word.replace(/na$/, 'ti hun');
           default:
             throw new Error(GENDER_ERROR);
         }
-      case 'past':
-        switch (gender) {
-          case 'm':
+      case 'Past':
+        switch (info.gender) {
+          case 'male':
             return word.replace(/na$/, 'ta tha');
-          case 'f':
+          case 'female':
             return word.replace(/na$/, 'ti thi');
           default:
             throw new Error(GENDER_ERROR);
         }
-      case 'future':
-        switch (gender) {
-          case 'm':
+      case 'Future':
+        switch (info.gender) {
+          case 'male':
             return word.replace(/na$/, 'nga');
-          case 'f':
+          case 'female':
             return word.replace(/na$/, 'ungi');
           default:
             throw new Error(GENDER_ERROR);
--- test/hindi_tests.js
+++ test/hindi_tests.js
@@ -5,213 +5,206 @@ import { Hindi } from '../src/hindi';
 
 describe('Hindi', () => {
   describe('Present tense masculine', () => {
-    it('should conjugate hindi present tense masculine', () => {
+    it('should conjugate hindi Present tense masculine', () => {
       let word;
       const ic = new Hindi();
 
-      word = ic.conjugate('nachna', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('nachna', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('nachta hun');
 
-      word = ic.conjugate('khana', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('khana', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('khata hun');
 
-      word = ic.conjugate('peena', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('peena', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('peeta hun');
 
-      word = ic.conjugate('khelna', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('khelna', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('khelta hun');
 
-      word = ic.conjugate('baat karna', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('baat karna', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('baat karta hun');
 
-      word = ic.conjugate('rukna', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('rukna', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('rukta hun');
 
-      word = ic.conjugate('padhna', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('padhna', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('padhta hun');
 
-      word = ic.conjugate('kam karna', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('kam karna', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('kam karta hun');
 
-      word = ic.conjugate('sona', {tense: 'present', gender: 'm'});
+      word = ic.conjugate('sona', {tense: 'Present', gender: 'male'});
       expect(word).to.equal('sota hun');
     });
   });
 
   describe('Present tense feminine', () => {
-    it('should conjugate hindi present tense feminine', () => {
+    it('should conjugate hindi Present tense feminine', () => {
       let word;
       const ic = new Hindi();
 
-      word = ic.conjugate('nachna', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('nachna', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('nachti hun');
 
-      word = ic.conjugate('khana', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('khana', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('khati hun');
 
-      word = ic.conjugate('peena', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('peena', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('peeti hun');
 
-      word = ic.conjugate('khelna', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('khelna', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('khelti hun');
 
-      word = ic.conjugate('baat karna', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('baat karna', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('baat karti hun');
 
-      word = ic.conjugate('rukna', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('rukna', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('rukti hun');
 
-      word = ic.conjugate('padhna', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('padhna', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('padhti hun');
 
-      word = ic.conjugate('kam karna', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('kam karna', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('kam karti hun');
 
-      word = ic.conjugate('sona', {tense: 'present', gender: 'f'});
+      word = ic.conjugate('sona', {tense: 'Present', gender: 'female'});
       expect(word).to.equal('soti hun');
     });
   });
 
   describe('Past tense masculine', () => {
-    it('should conjugate hindi past tense masculine', () => {
+    it('should conjugate hindi Past tense masculine', () => {
       let word;
       const ic = new Hindi();
 
-      word = ic.conjugate('nachna', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('nachna', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('nachta tha');
 
-      word = ic.conjugate('khana', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('khana', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('khata tha');
 
-      word = ic.conjugate('peena', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('peena', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('peeta tha');
 
-      word = ic.conjugate('khelna', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('khelna', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('khelta tha');
 
-      word = ic.conjugate('baat karna', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('baat karna', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('baat karta tha');
 
-      word = ic.conjugate('rukna', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('rukna', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('rukta tha');
 
-      word = ic.conjugate('padhna', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('padhna', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('padhta tha');
 
-      word = ic.conjugate('kam karna', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('kam karna', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('kam karta tha');
 
-      word = ic.conjugate('sona', {tense: 'past', gender: 'm'});
+      word = ic.conjugate('sona', {tense: 'Past', gender: 'male'});
       expect(word).to.equal('sota tha');
     });
   });
 
   describe('Past tense feminine', () => {
-    it('should conjugate hindi past tense feminine', () => {
+    it('should conjugate hindi Past tense feminine', () => {
       let word;
       const ic = new Hindi();
 
-      word = ic.conjugate('nachna', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('nachna', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('nachti thi');
 
-      word = ic.conjugate('khana', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('khana', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('khati thi');
 
-      word = ic.conjugate('peena', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('peena', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('peeti thi');
 
-      word = ic.conjugate('khelna', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('khelna', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('khelti thi');
 
-      word = ic.conjugate('baat karna', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('baat karna', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('baat karti thi');
 
-      word = ic.conjugate('rukna', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('rukna', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('rukti thi');
 
-      word = ic.conjugate('padhna', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('padhna', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('padhti thi');
 
-      word = ic.conjugate('kam karna', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('kam karna', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('kam karti thi');
 
-      word = ic.conjugate('sona', {tense: 'past', gender: 'f'});
+      word = ic.conjugate('sona', {tense: 'Past', gender: 'female'});
       expect(word).to.equal('soti thi');
     });
   });
 
   describe('Future tense masculine', () => {
-    it.skip('should conjugate hindi future tense masculine', () => {
+    it.skip('should conjugate hindi Future tense masculine', () => {
       let word;
       const ic = new Hindi();
 
-      word = ic.conjugate('nachna', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('nachna', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('nachunga');
 
-      word = ic.conjugate('khana', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('khana', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('khaunga');
 
-      word = ic.conjugate('peena', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('peena', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('peeunga');
 
-      word = ic.conjugate('khelna', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('khelna', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('khelunga');
 
-      word = ic.conjugate('baat karna', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('baat karna', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('baat karunga');
 
-      word = ic.conjugate('rukna', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('rukna', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('rukunga');
 
-      word = ic.conjugate('padhna', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('padhna', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('padhunga');
 
-      word = ic.conjugate('kam karna', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('kam karna', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('kam karunga');
 
-      word = ic.conjugate('sona', {tense: 'future', gender: 'm'});
+      word = ic.conjugate('sona', {tense: 'Future', gender: 'male'});
       expect(word).to.equal('sounga');
     });
   });
 
   describe('Future tense feminine', () => {
-    it('should conjugate hindi future tense feminine', () => {
+    it('should conjugate hindi Future tense feminine', () => {
       let word;
       const ic = new Hindi();
 
-      word = ic.conjugate('nachna', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('nachna', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('nachungi');
 
-      word = ic.conjugate('khana', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('khana', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('khaungi');
 
-      word = ic.conjugate('peena', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('peena', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('peeungi');
 
-      word = ic.conjugate('khelna', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('khelna', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('khelungi');
 
-      word = ic.conjugate('baat karna', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('baat karna', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('baat karungi');
 
-      word = ic.conjugate('rukna', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('rukna', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('rukungi');
 
-      word = ic.conjugate('padhna', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('padhna', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('padhungi');
 
-      word = ic.conjugate('kam karna', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('kam karna', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('kam karungi');
 
-      word = ic.conjugate('sona', {tense: 'future', gender: 'f'});
+      word = ic.conjugate('sona', {tense: 'Future', gender: 'female'});
       expect(word).to.equal('soungi');
-    });
-  });
-  describe('setInfo', () => {
-    it('should return the info adapted for the conjugator', () => {
-      const ic = new Hindi();
-      let infos = ic.setInfo({Tense: 'Present', Gender: 'Female', Formal: 'true', 'Word Type': 'Adjective'});
-      expect(infos).to.deep.equal({tense: 'present', gender: 'f', formal: true, wordType: 'adjective'});
     });
   });
 });
