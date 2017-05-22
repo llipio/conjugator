7998e64af419189f0a9d36dd73047b2ebf26ce0b
Betty Chow <bettychchow@gmail.com>
2017-05-22 15:42:59 -0700

    combining 6 commits CSS for conjugator

--- src/components/App.js
+++ src/components/App.js
@@ -25,26 +25,60 @@ const App = ({options, selectedLanguage, changeLanguage, word, changeWord, langu
     }}
 
   return (
+
     <div className="App">
-      <div>
-        <h1>Select Your Language!</h1>
+      <div className="title-container">
+        <span className="title" >CONJUGATOR</span>
       </div>
+      <div className='content-container'>
+        <p className="instruction_text">Select Your Language!</p>
+        <div className="dropdown">
+          <Dropdown 
+          options={languageList} onChange={changeLanguage} 
+          value={selectedLanguage} placeholder="Please Select Language" 
+          />
+        </div>
+         <div className="radio option_text">
+           <label className="button">
+             <input type="radio" /> Option 1
+           </label>
+           <label className="button">
+             <input type="radio" /> Option 2
+           </label>
+           <label className="button">
+             <input type="radio" /> Option 3
+           </label>
+           <label className="button">
+             <input type="radio" /> Option 4
+           </label>
+           <label className="button">
+             <input type="radio" /> Option 5
+           </label>
+           <label className="button">
+             <input type="radio" /> Option 6
+           </label>
+         </div>
 
       <Dropdown options={languageList} onChange={(languageOption) => {changeLanguage(languageOption.value)}} value={selectedLanguage} placeholder="Please Select Language" />
       {optionListForm} {optionBulletForm}
       <div>
-        <input type="text" name="word" placeholder="Please input word" value={word} onChange={(e) => {
+        <input type="text" className="input-box" name="word" placeholder="Please input word" value={word} onChange={(e) => {
           changeWord(e.target.value);
         }} />
     </div>
 
-    <div>
-      <button type="button" className="btn btn-primary" onClick={submit}>Submit</button>
-    </div>
-
     <h1>{conjugatedWord}</h1>
-  </div>
-  );
+        <p className="instruction_text">Type Your Word</p>
+      <div>
+        <img src="/arrow_button_white.png"
+          width="73" height="auto" onClick={ () => { submit(); } } />
+        <input type="text" className="output-box"/>
+      </div>
+    </div>  
+ </div>
+
+
+);
 }
 
 export default App;
