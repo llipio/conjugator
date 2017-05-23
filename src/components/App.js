3b72b2760c88d01b490e6279cbb77a55169923d6
Betty Chow <bettychchow@gmail.com>
2017-05-22 17:17:41 -0700

    CSS for conjugator comments fixed

--- src/components/App.js
+++ src/components/App.js
@@ -7,11 +7,11 @@ const RadioButtonOptions = ({options, setOptions, title}) => {
   let radioOptions = [];
     options.map(element => {
       radioOptions.push(
-        <label key={element}>
+        <label className="button" key={element}>
           <input type="radio" name={title} value={element} onChange={() => {setOptions(title, element)}}/>{`${element}`}
         </label>)
     })
-  return (<div>{title}{radioOptions}</div>);
+  return (<div className="radio option_text">{title}{radioOptions}</div>);
   }
 
 const App = ({options, selectedLanguage, changeLanguage, word, changeWord, languageList, submit, setOptions, info, conjugatedWord}) => {
@@ -32,48 +32,17 @@ const App = ({options, selectedLanguage, changeLanguage, word, changeWord, langu
       </div>
       <div className='content-container'>
         <p className="instruction_text">Select Your Language!</p>
-        <div className="dropdown">
-          <Dropdown 
-          options={languageList} onChange={changeLanguage} 
-          value={selectedLanguage} placeholder="Please Select Language" 
-          />
-        </div>
-         <div className="radio option_text">
-           <label className="button">
-             <input type="radio" /> Option 1
-           </label>
-           <label className="button">
-             <input type="radio" /> Option 2
-           </label>
-           <label className="button">
-             <input type="radio" /> Option 3
-           </label>
-           <label className="button">
-             <input type="radio" /> Option 4
-           </label>
-           <label className="button">
-             <input type="radio" /> Option 5
-           </label>
-           <label className="button">
-             <input type="radio" /> Option 6
-           </label>
-         </div>
-
       <Dropdown options={languageList} onChange={(languageOption) => {changeLanguage(languageOption.value)}} value={selectedLanguage} placeholder="Please Select Language" />
       {optionListForm} {optionBulletForm}
-      <div>
-        <input type="text" className="input-box" name="word" placeholder="Please input word" value={word} onChange={(e) => {
+        <p className="instruction_text">Type Your Word</p>    
+    <input type="text" className="input-box" name="word" placeholder="Please input word" value={word} onChange={(e) => {
           changeWord(e.target.value);
         }} />
-    </div>
-
-    <h1>{conjugatedWord}</h1>
-        <p className="instruction_text">Type Your Word</p>
-      <div>
-        <img src="/arrow_button_white.png"
+        <img className="image" src="/arrow_button_white.png"
           width="73" height="auto" onClick={ () => { submit(); } } />
-        <input type="text" className="output-box"/>
-      </div>
+        <label>
+          <input className="output-box" type="textbox" name="output" value={conjugatedWord} />
+        </label>
     </div>  
  </div>
 
