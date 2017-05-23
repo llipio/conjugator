dcf16a63904d83b6d6fb2c9219c39a5a5953449b
trifrog <tri.d.le@live.com>
2017-05-22 18:43:48 -0700

    Adjusted reference object for conjugation

--- src/spanish.js
+++ src/spanish.js
@@ -1,65 +1,84 @@
-const presentTenseConjugationMap = {
-  ar: {
-    yo: 'o',
-    tú: 'as',
-    'él/ella/Ud.': 'a',
-    'nosotros/nosotras': 'amos',
-    'vosotros/vosotras': 'áis',
-    'ellos/ellas/Uds.': 'an'
+const regularVerbConjugation = {
+  present: {
+    ar: {
+      yo: 'o',
+      tú: 'as',
+      'él/ella/Ud.': 'a',
+      'nosotros/nosotras': 'amos',
+      'vosotros/vosotras': 'áis',
+      'ellos/ellas/Uds.': 'an'
+    },
+    er: {
+      yo: 'o',
+      tú: 'es',
+      'él/ella/Ud.': 'e',
+      'nosotros/nosotras': 'emos',
+      'vosotros/vosotras': 'éis',
+      'ellos/ellas/Uds.': 'en'
+    },
+    ir: {
+      yo: 'o',
+      tú: 'es',
+      'él/ella/Ud.': 'e',
+      'nosotros/nosotras': 'imos',
+      'vosotros/vosotras': 'ís',
+      'ellos/ellas/Uds.': 'en'
+    }
   },
-  er: {
-    yo: 'o',
-    tú: 'es',
-    'él/ella/Ud.': 'e',
-    'nosotros/nosotras': 'emos',
-    'vosotros/vosotras': 'éis',
-    'ellos/ellas/Uds.': 'en'
+  future: {
+    ar: {
+      yo: 'é',
+      tú: 'ás',
+      'él/ella/Ud.': 'á',
+      'nosotros/nosotras': 'emos',
+      'vosotros/vosotras': 'éis',
+      'ellos/ellas/Uds.': 'án'
+    },
+    er: {
+      yo: 'é',
+      tú: 'ás',
+      'él/ella/Ud.': 'á',
+      'nosotros/nosotras': 'emos',
+      'vosotros/vosotras': 'éis',
+      'ellos/ellas/Uds.': 'án'
+    },
+    ir: {
+      yo: 'é',
+      tú: 'ás',
+      'él/ella/Ud.': 'á',
+      'nosotros/nosotras': 'emos',
+      'vosotros/vosotras': 'éis',
+      'ellos/ellas/Uds.': 'án'
+    }
   },
-  ir: {
-    yo: 'o',
-    tú: 'es',
-    'él/ella/Ud.': 'e',
-    'nosotros/nosotras': 'imos',
-    'vosotros/vosotras': 'ís',
-    'ellos/ellas/Uds.': 'en'
+  past: {
+    ar: {
+      yo: 'é',
+      tú: 'aste',
+      'él/ella/Ud.': 'ó',
+      'nosotros/nosotras': 'amos',
+      'vosotros/vosotras': 'asteis',
+      'ellos/ellas/Uds.': 'aron'
+    },
+    er: {
+      yo: 'í',
+      tú: 'iste',
+      'él/ella/Ud.': 'ió',
+      'nosotros/nosotras': 'imos',
+      'vosotros/vosotras': 'isteis',
+      'ellos/ellas/Uds.': 'ieron'
+    },
+    ir: {
+      yo: 'í',
+      tú: 'iste',
+      'él/ella/Ud.': 'ió',
+      'nosotros/nosotras': 'imos',
+      'vosotros/vosotras': 'isteis',
+      'ellos/ellas/Uds.': 'ieron'
+    }
   }
 };
 
-const futureTenseConjugationMap = {
-  yo: 'é',
-  tú: 'ás',
-  'él/ella/Ud.': 'á',
-  'nosotros/nosotras': 'emos',
-  'vosotros/vosotras': 'éis',
-  'ellos/ellas/Uds.': 'án'
-};
-
-const pastTenseConjugationMap = {
-  ar: {
-    yo: 'é',
-    tú: 'aste',
-    'él/ella/Ud.': 'ó',
-    'nosotros/nosotras': 'amos',
-    'vosotros/vosotras': 'asteis',
-    'ellos/ellas/Uds.': 'aron'
-  },
-  er: {
-    yo: 'í',
-    tú: 'iste',
-    'él/ella/Ud.': 'ió',
-    'nosotros/nosotras': 'imos',
-    'vosotros/vosotras': 'isteis',
-    'ellos/ellas/Uds.': 'ieron'
-  },
-  ir: {
-    yo: 'í',
-    tú: 'iste',
-    'él/ella/Ud.': 'ió',
-    'nosotros/nosotras': 'imos',
-    'vosotros/vosotras': 'isteis',
-    'ellos/ellas/Uds.': 'ieron'
-  }
-};
 const allInfo = {
   tense: ['present', 'future', 'past'],
   pronoun: ['yo', 'tú', 'él/ella/Ud.', 'nosotros/nosotras', 'vosotros/vosotras', 'ellos/ellas/Uds.']
@@ -67,27 +86,14 @@ const allInfo = {
 
 class Spanish {
   conjugate (word, info) {
-    const ending = word.slice(word.length - 2, word.length);
+    const ending = word.slice(-2);
     const stem = word.slice(0, -2);
-    const ERROR_MESSAGE = {
-      FOR_TENSE: 'Please select a valid tense: present, future, past.',
-      FOR_ENDING: 'Please enter a word with valid ending: ar, er, ir.'
-    };
+    const ERROR_MESSAGE = 'Unable to conjugate';
 
     if (ending === 'ar' || ending === 'er' || ending === 'ir') {
-      switch (info.tense) {
-        case 'present':
-          return stem.concat(presentTenseConjugationMap[ending][info.pronoun]);
-        case 'future':
-          return word.concat(futureTenseConjugationMap[info.pronoun]);
-        case 'past':
-          return stem.concat(pastTenseConjugationMap[ending][info.pronoun]);
-        default:
-          return ERROR_MESSAGE.FOR_TENSE;
-      }
-    } else {
-      return ERROR_MESSAGE.FOR_ENDING;
+      return stem.concat(regularVerbConjugation[ending][info.pronoun]);
     }
+    return ERROR_MESSAGE;
   }
 
   getInfoList () {
