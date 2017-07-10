e964e992a53203f2d3dc9fd66c57b5a78020f96d
Yoo Jin Lim <yjlim5@gmail.com>
2017-07-10 12:00:49 -0700

    update conjugator web to reflect new conjugator library

--- src/components/App.js
+++ src/components/App.js
@@ -4,41 +4,54 @@ import '../style/ReactDropdown.css';
 import Dropdown from 'react-dropdown';
 import myImage from '../assets/arrow_button_white.png';
 
-const RadioButtonOptions = ({options, setOptions, title}) => {
+const RadioButtonOptions = ({options, setOptions, displayTitle, title, selectedOption}) => {
   let radioOptions = [];
-    options.map(element => {
-      radioOptions.push(
-        <label className="button" key={element}>
-          <input type="radio" name={title} value={element} onChange={() => {setOptions(title, element)}}/>{`${element}`}
-        </label>);
+  options.map(element => {
+    radioOptions.push(
+      <label className="button" key={element}>
+        <input type="radio" 
+          name={title}
+          value={element}
+          onChange={() => {setOptions(title, element)}}
+          checked={element === selectedOption}
+        />
+      {`${element}`}
+      </label>
+    );
     })
-  return (<div className="radio-group"><div className="radio-title">{title}</div><span>{radioOptions}</span></div>);
+  return (<div className="radio-group"><div className="radio-title">{displayTitle || title}</div><span>{radioOptions}</span></div>);
   }
 
 const App = ({options, selectedLanguage, changeLanguage, word, changeWord, languageList, submit, setOptions, info, conjugatedWord}) => {
   let wordTypeForm = '';
   let optionListForm = [];
   let optionBulletForm = [];
-  console.log('component options:', options);
   wordTypeForm =
     <RadioButtonOptions
       options={Object.keys(options)}
       setOptions={setOptions}
-      title='WordType'
+      displayTitle='Word Type'
+      title='wordType'
+      selectedOption={info.wordType}
     />
-  console.log('info:', info);
-  let chosenType = info.WordType || 'verb';
+  let chosenType = info.wordType || 'verb';
   for(let title in options[chosenType]) {
     if (options[chosenType][title].length > 2){
       optionListForm.push(
         <div key={title} className="option">
           <div className="option-title">{title}</div>
-            <div className="option-menu">
-              <Dropdown value={options[chosenType][title]} options={options[chosenType][title]} onChange={(tense) => {setOptions(title,tense.value)}}/>
-            </div>
+          <div className="option-menu">
+            <Dropdown
+              value={info[title]}
+              options={options[chosenType][title]}
+              onChange={(tense) => {setOptions(title,tense.value)}}
+            />
+          </div>
         </div>)
     } else {
-      optionBulletForm.push(<RadioButtonOptions key={title} options={options[chosenType][title]} setOptions={setOptions} title={title}/>);
+      optionBulletForm.push(<RadioButtonOptions key={title} options={options[chosenType][title]} setOptions={setOptions} 
+      selectedOption={info[title]}
+      title={title}/>);
     }}
 
   return (
